c2661932dff44106370214f535087b4d22a6c15f
muhamadbasim <muhamadbasim@gmail.com>
2024-06-06 18:27:47 +0700

    final

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-// ini javascriptt
+// Ini Javascript//
+
+function replaceName () {
+    let name = prompt("Selamat datang di kampus juara! sebelum masuk ketik dulu yuk nama kamu", "");
+    document.getElementById("name").innerHTML = name
+}
+
+replaceName()
+
+
+
+
+function validateForm() {
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
+
+    if (name == "" || birthDate == "" || gender == "" || messages == "") {
+        alert("Tidak Boleh ada yang kosong");
+        return false;
+    }
+    
+    setSenderUI(name, birthDate, gender, messages); 
+
+    return false
+}
+
+function setSenderUI(name, birthDate, gender, messages) {
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
+}
